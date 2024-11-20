7e4fe2a20f3937a4bf258f2c9e0d5a96db9763a8
abdulwahababdal <abdulwahababdal@gmail.com>
2024-11-20 09:31:22 +0300

    done with token

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,25 +1,37 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION_MS, JWT_SECRET } = require("../config/keys");
 
-exports.signup = async (req, res) => {
+exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const saltRounds = 10;
+    const hashPassword = bcrypt.hash(req.body.password, saltRounds);
+    const newUser = await User.create({
+      username: req.body.username,
+      password: hashPassword,
+    });
+
+    res.status(201).json({ message: "Successfully created User" });
   } catch (err) {
     next(err);
   }
 };
 
-exports.signin = async (req, res) => {
+exports.signin = async (req, res, next) => {
   try {
-  } catch (err) {
-    res.status(500).json("Server Error");
-  }
-};
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ meesage: "Invaild credetials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ meesage: "Invaild credetials" });
+    }
 
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find().populate("urls");
-    res.status(201).json(users);
+    const token = generateToken(user);
+    res.status(200).json({ token, message: "Signed in succesfully" });
   } catch (err) {
     next(err);
   }
